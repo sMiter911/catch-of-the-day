0c9d3aa82eb9b16a506d1701868728198fd6ad81
sMiter911 <wandumuzi.m@sysinfo.co.za>
2020-11-22 09:55:15 +0200

    Added order state functionality;Changes usd to zar in currency

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header'
 import Inventory from './Inventory'
 import Order from './Order'
 import sampleFishes from '../sample-fishes'
+import Fish from './Fish';
 
 class App extends React.Component {
   state = {
@@ -12,28 +13,46 @@ class App extends React.Component {
 
   addFish = (fish) => {
     // 1. Take a copy of the existing state
-    const fishes = {...this.state.fishes};
+    const fishes = { ...this.state.fishes };
     // 2. Add our new fish to the fishes variables
     fishes[`fish${Date.now()}`] = fish;
     // 3. Set the new fishes object to state
     this.setState({
       fishes
     });
-  }
-
+  };
   loadSampleFishes = () => {
-   this.setState({fishes: sampleFishes})
+    this.setState({ fishes: sampleFishes })
+  };
+
+  addToOrder = (key) => {
+    // 1. Take a copy of the state
+    const order = { ...this.state.order };
+    // 2. Either add to the order or update the order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call the state to update the state object
+    this.setState(
+      { order }
+    );
   }
 
   render() {
     return (
-    <div className="catch-of-the-day">
-      <div className="menu">
-        <Header tagline="Fresh Seafood Market"></Header>
-      </div>
-      <Order></Order>
-      <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}></Inventory>
-    </div>)
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market"></Header>
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder} />))}
+          </ul>
+        </div>
+        <Order></Order>
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}></Inventory>
+      </div>)
   }
 }
 
--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,7 @@
 export function formatPrice(cents) {
-  return (cents / 100).toLocaleString("en-US", {
+  return (cents / 100).toLocaleString("en-ZA", {
     style: "currency",
-    currency: "AR"
+    currency: "ZAR"
   });
 }
 
